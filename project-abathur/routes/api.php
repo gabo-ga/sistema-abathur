f23bdb53c9138de23c92022ca407a2048dcdf020
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2025-02-18 09:44:41 -0400

    feat: Agregar rutas para los modelos Lote y MovimientoStock en api.php

--- project-abathur/routes/api.php
+++ project-abathur/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MedicamentosController;
 use App\Http\Controllers\ProveedorController;
+use App\Http\Controllers\LoteController;
+use App\Http\Controllers\MovimientoStockController;
 
 Route::get('/medicamentos', [MedicamentosController::class, 'index']);
 Route::get('/medicamentos/{id}', [MEdicamentosController::class, 'show']);
@@ -18,6 +20,16 @@ Route::post('/proveedores', [ProveedorController::class, 'store']);
 Route::put('/proveedores/{id}', [ProveedorController::class, 'update']);
 Route::delete('/proveedores/{id}', [ProveedorController::class, 'destroy']);
 
+Route::get('/lote', [LoteController::class, 'index']);
+Route::get('/lote/{id}', [LoteController::class, 'show']);
+Route::post('/lote', [LoteController::class, 'store']);
+Route::put('/lote/{id}', [LoteController::class, 'update']);
+Route::delete('/lote/{id}', [LoteController::class, 'destroy']);
 
+Route::get('/movimientos', [MovimientoStockController::class, 'index']);
+Route::get('/movimientos/{id}', [MovimientoStockController::class, 'show']);
+Route::post('/movimientos', [MovimientoStockController::class, 'store']);
+Route::put('/movimientos/{id}', [MovimientoStockController::class, 'update']);
+Route::delete('/movimientos/{id}', [MovimientoStockController::class, 'destroy']);
 
 ?>
